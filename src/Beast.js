7cac55554dc566d07d74fe4b251af4d69c2b15ba
JJ Escandor <jj.p.escandor@gmail.com>
2022-03-02 00:50:59 -0500

    Finished Lab 02

--- src/Beast.js
+++ src/Beast.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Beast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: this.props.title,
+            imgUrl: this.props.imgUrl,
+            description: this.props.description,
+            likes: 0
+        }
+    }
+    likes = () => {
+        this.setState({
+            likes: this.state.likes + 1
+        })
+    }
+    resetLikes = () => {
+        this.setState({
+            likes: 0
+        })
+    }
     render() {
         return (
-            <>
-                <h1>{this.props.title}</h1>
-                <img src={this.props.imgUrl} alt={this.props.title} title={this.props.title}></img>
-                <p>{this.props.description}</p>
-            </>
+            <Card style={{ fontWeight: 'bold', padding: '30px' }} >
+                <Card.Img onClick={this.likes} src={this.state.imgUrl} alt={this.state.title} style={{ width: '500px', borderRadius: '1%', boxShadow: '0 15px 10px -10px #777', cursor: 'pointer' }} />
+                <Card.Body>
+                    <Card.Title>{this.state.title}</Card.Title>
+                    <Card.Text style={{ color: 'red' }}> {this.state.likes ? `♥️ ${this.state.likes} ♥️ ` : "Please ♥️  me"} </Card.Text>
+                    <Card.Text>
+                        {this.state.description}
+                    </Card.Text>
+                    <Button onClick={this.resetLikes}>Reset ♥️</Button>
+                </Card.Body>
+            </ Card>
         )
     }
 }
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return (
             <>
-                <h1>Horned Beast</h1>
+                <h1 style={{ padding: '50px' }}>Horned Beast</h1>
             </>
         )
     }
