7649e2729008fb55518a57357ea21b1051235343
JJ Escandor <jj.p.escandor@gmail.com>
2022-03-15 21:18:28 -0400

    added build a beast functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import SelectedBeast from './SelectedBeast';
-import BeastsArr from './data.json';
+import BeastsArrJSON from './data.json';
 import SearchBeastForm from './SearchBeastForm';
 import CreateBeast from './CreateBeast';
 
@@ -19,8 +19,8 @@ class App extends React.Component {
       beastName: null,
       beastDescription: null,
       beastImgUrl: null,
-      beastData: BeastsArr,
-      newBeastArr: JSON.parse(localStorage.getItem('beast')) || []
+      newBeastArr: [],
+      beastArr: JSON.parse(localStorage.getItem('beast')) ? [...BeastsArrJSON, ...JSON.parse(localStorage.getItem('beast'))] : BeastsArrJSON
     };
     this.searchBeast = this.searchBeast.bind(this);
     this.showModal = this.showModal.bind(this);
@@ -29,12 +29,12 @@ class App extends React.Component {
 
   showBuildModal = () => {
     this.setState({ showCreate: true });
+    console.log(this.state.newBeastArr);
   }
 
   buildBeast = (newBeast) => {
-    console.log(newBeast)
     this.state.newBeastArr.push(newBeast);
-    this.setState({ beastData: [...BeastsArr, ...this.state.newBeastArr] });
+    this.setState({ beastArr: [...this.state.beastArr, ...this.state.newBeastArr] });
     localStorage.setItem('beast', JSON.stringify(this.state.newBeastArr))
 
   }
@@ -62,13 +62,13 @@ class App extends React.Component {
   searchBeast = (keyWord) => {
     let re = new RegExp(keyWord, 'i')
     this.setState({
-      beastData: BeastsArr.filter(beast => beast.title.match(re) || beast.description.match(re))
+      beastArr: this.beastArr.filter(beast => beast.title.match(re) || beast.description.match(re))
     })
   }
 
   filterHorns = (numHorns) => {
-    numHorns !== 'all' ? this.setState({ beastData: BeastsArr.filter(beast => beast.horns === parseInt(numHorns)) })
-      : this.setState({ beastData: BeastsArr })
+    numHorns !== 'all' ? this.setState({ beastArr: this.state.beastArr.filter(beast => beast.horns === parseInt(numHorns)) })
+      : this.setState({ beastArr: this.state.beastArr })
   }
 
   render() {
@@ -78,7 +78,7 @@ class App extends React.Component {
         <CreateBeast showCreate={this.state.showCreate} hideBuildBeastModal={this.hideBuildBeastModal} buildBeast={this.buildBeast} />
         <SearchBeastForm search={this.searchBeast} filterHorns={this.filterHorns} />
         <SelectedBeast handleClose={this.onHide} show={this.state.show} name={this.state.beastName} description={this.state.beastDescription} url={this.state.beastImgUrl} />
-        <Main showModal={this.showModal} beastArr={this.state.beastData} />
+        <Main showModal={this.showModal} beastArr={this.state.beastArr} />
         <Footer />
       </div >
     );
